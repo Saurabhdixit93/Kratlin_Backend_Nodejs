810125319cd30f8314cb3b308bafab176922016e
Saurabh Dixit <86460997+Saurabhdixit93@users.noreply.github.com>
2023-08-10 17:47:15 +0000

    Update generateToken.js

--- middlewares/generateToken.js
+++ middlewares/generateToken.js
@@ -2,10 +2,8 @@ const jwt = require("jsonwebtoken");
 
 const secretKey = process.env.SECRET_KEY;
 
-const generateJwtToken = async (userId, name, email) => {
+module.exports.generateJwtToken = async (userId, name, email) => {
   return await jwt.sign({ userId, name, email }, secretKey, {
     expiresIn: "48h",
   });
 };
-
-module.exports = generateJwtToken;
